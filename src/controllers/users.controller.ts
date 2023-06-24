4c6ecaf3d397645c9caad14b6d5ed8db4ec5a280
Destiny <onahdestinyc20@gmail.com>
2023-06-24 01:22:43 +0100

    improves

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -38,6 +38,7 @@ export const registerUser: RequestHandler = async (req, res) => {
       expiresIn: "1d"
     });
     userToCreate.token = generateToken;
+    await userToCreate.save();
     const verifyAccountRoute = `${req.protocol}://${req.get("host")}/api/v1/verify/${userToCreate.id}`;
     const message = `Hello cheif ${userToCreate.fullname} Kindly use the link to verify your account  ${verifyAccountRoute}`;
     const mailservice = new mailSender();
@@ -48,7 +49,6 @@ export const registerUser: RequestHandler = async (req, res) => {
       subject: "Kindly verify!",
       message
     });
-    await userToCreate.save();
     res.status(201).json({
       message: "Created Successfully.",
       data: userToCreate
@@ -122,7 +122,64 @@ export const verifyUser: RequestHandler = async (req, res) => {
 
 export const forgottenPassword: RequestHandler = async (req, res) => {
   try {
+    const { email } = req.body;
+    const validEmail = await User.findOne({ where: { email: email } });
+    if (!validEmail) {
+      return res.status(400).json({
+        message: "Email inputed does not exist!"
+      })
+    }
+    const regexValidation = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    const validateEmail = email.match(regexValidation);
+    if (!validateEmail) {
+      return res.status(400).json({
+        message: "Invalid Email format!"
+      })
+    }
+    const changePasswordRoute = `${req.protocol}://${req.get("host")}/api/v1/change/${validEmail.id}`;
+    const message = `Hello ${validEmail.fullname} you required for a change of password, 
+    here use this link to change your password ${changePasswordRoute}`
+    const mailServiceInstance = new mailSender();
+    mailServiceInstance.createConnection();
+    mailServiceInstance.mail({
+      from: process.env.EMAIL,
+      email: validEmail.email,
+      subject: "Forgotten Password!",
+      message
+    });
+    return res.status(200).json({
+      message: "Please check your mail for forgotten password mail!"
+    })
+  } catch (error: any) {
+    return res.status(500).json({
+      message: error.message,
+      status: "Failed"
+    })
+  }
+};
 
+export const changePasswordUser: RequestHandler = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const { password, confirmPassword } = req.body;
+    const matchedPassword = password.match(confirmPassword);
+    if (!matchedPassword) {
+      return res.status(400).json({
+        message: "Password does not match, please check and confirm!"
+      })
+    };
+    const saltPassword = await bcrypt.genSalt(9);
+    const hassPassword = await bcrypt.hash(password, saltPassword);
+    await User.update(
+      { password: hassPassword },
+      {
+        where: {
+          id: userId
+        }
+      })
+    return res.status(200).json({
+      message: "Password changed successful!"
+    })
   } catch (error: any) {
     return res.status(500).json({
       message: error.message,
--- src/routers/user.route.ts
+++ src/routers/user.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { loginValidate, validateUser } from "../middlewares/validates";
-import { loginUser, registerUser, verifyUser } from "../controllers/users.controller";
+import { changePasswordUser, forgottenPassword, loginUser, registerUser, verifyUser } from "../controllers/users.controller";
 import { userSchema } from "../schemas/user.schema";
 import { adminLogin } from "../schemas/admin.schema";
 
@@ -10,5 +10,7 @@ const userRoute = Router();
 userRoute.route("/user/register").post(validateUser(userSchema), registerUser);
 userRoute.route("/user/login").post(loginValidate(adminLogin), loginUser);
 userRoute.route("/user/verify/:userId").patch(verifyUser);
+userRoute.route("/user/forgot").post(forgottenPassword)
+userRoute.route("/user/change/:userId").patch(changePasswordUser)
 
 export default userRoute;
